f893518ccd8f15a69078902844cd67563ab42cd7
hiddeagema <hiddeagema@hotmail.com>
2017-11-10 17:10:14 +0100

    ksdskc

--- les03/final assignment 10.py
+++ les03/final assignment 10.py
@@ -1,40 +1,21 @@
-def import xmltodict
-def processXML(filename):
-    with open(filename) as myXMLFILE:
-        filestring = myXMLFILE.read()
-        xmldictionary = xmltodict.parse(filestring)
-        return xmldictionary
-stationsdict = processXML('FA10.xml')
-stations = stationsdict['Stations']['Station']
-code = station['code']
-type = station['type']
-synoniemen = station['Synoniemen']
-for station in stations:
-    if station['Synoniemen'] is not None:
-        print('{:4} - {}'.format(station['Code'],station['Type'],station['Synoniemen']))
-
-def import xmltodict
-def processXML(filename):
-    with open(filename) as myXMLFILE:
-        filestring = myXMLFILE.read()
-        xmldictionary = xmltodict.parse(filestring)
-        return xmldictionary
-stationsdict = processXML('FA10.xml')
-stations = stationsdict['Stations']['Station']
-code = station['code']
-type = station['type']
-synoniemen = station['Synoniemen']
-for station in stations:
-    print('{:4} - {}'.format(station['Code'],station['Type']))
-
 import xmltodict
-xmlfile = xmltodict.parse(open("xml_stationslijst.xml").read())
-print("Dit zijn de codes en types van de 4 stations:")
-for station in xmlfile['Stations']['Station']: print(station['Code'],"-",station['Type'])
-    print("\nDit zijn alle stations met een of meer synoniemen:")
-for station in xmlfile['Stations']['Station']:
-    if station['Synoniemen'] != None and len(station['Synoniemen']['Synoniem']) > 1:
-        print(station['Code'],"-",station['Synoniemen'])
-        print("\nDit is de lange naam van elk station:")#
-for station in xmlfile['Stations']['Station']:
-    print(station['Code'],"-",station['Namen']['Lang'])
+def processXML(filename):
+    with open(filename)as myXMLFile:
+        filestring = myXMLFile.read()
+        xmldictionary = xmltodict.parse(filestring)
+        return xmldictionary
+
+voorbeeldendict = processXML('FA10.xml')
+voorbeelden = voorbeeldendict['Stations']['Station']
+print('Dit zijn de codes en types van de {} stations'.format(len(voorbeelden)))
+for voorbeeld in voorbeelden:
+    print('{:4}- {}'.format(voorbeeld['Code'],voorbeeld['Type']))
+print()
+print('Dit zijn alle stations met een of meer synoniemen:')
+for voorbeeld in voorbeelden:
+    if voorbeeld['Synoniemen'] is not None:
+        print('{:4}- {}'.format(voorbeeld['Code'],voorbeeld['Synoniemen']))
+print()
+print('Dit is de lange naam van elk station:')
+for voorbeeld in voorbeelden:
+    print('{:4}- {}'.format(voorbeeld['Code'],voorbeeld['Namen']['Lang']))
